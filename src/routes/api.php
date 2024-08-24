eba7dba2ad2fb73d8265db2b418d27f1a1dee235
Henington <heningtonfrota@gmail.com>
2024-08-24 11:57:04 -0400

    login token

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,10 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\Auth\{
+    AuthController
+};
 use Illuminate\Support\Facades\Route;
 
 Route::get('version', fn() => response()->json(['version' => '1.0.0']));
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('auth/create-token', [AuthController::class, 'createToken']);
